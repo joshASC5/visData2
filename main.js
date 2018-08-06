261cbdf7e46fa6675c7235ef018b8b7fc958556d
josh <joshfeli613@gmail.com>
2018-08-06 17:06:50 -0400

    First two checkboxes work

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const numberOfUsersTextBox = document.getElementById("numberOfUsersTextBox")
 const checkboxes = document.querySelectorAll(".checkbox")
 for(const box of checkboxes){
-    box.setAttribute("aria-checked", "true")
+    box.setAttribute("checked", "true")
 }
 
 const male = document.getElementById("male")
@@ -12,11 +12,34 @@ const lastname = document.getElementById("lastname")
 const submitButton = document.getElementById("submit")
 submitButton.addEventListener("click", retrieveData)
 
+const resetButton = document.getElementById("reset")
+resetButton.addEventListener("click", resetNames)
+
 function retrieveData(){
-    const numberOfUsers = numberOfUsersTextBox.value
+    const numberOfUsers = Math.floor(Number(numberOfUsersTextBox.value))
+    const isMale = male.checked;
+    console.log(isMale)
+    const isFemale = female.checked;
+    console.log(isFemale)
     const baseUrl = "https://randomuser.me/api/?results="
-    const url = encodeURI(baseUrl + numberOfUsers)
-    fetch(url)
+    let url = encodeURI(baseUrl + numberOfUsers)
+    if(numberOfUsers <= 0 || numberOfUsers == NaN){
+        alert("Please type in a valid number.")
+    }
+
+    /*if(isMale == true && isFemale == true){
+    }*/
+    if(isMale !== true && isFemale !== true){
+        alert("You gotta pick a gender")
+    }
+    else {
+        if(isMale == true){
+            url += "&gender=male"
+        }
+        if(isFemale == true){
+            url += "&gender=female"
+        }
+        fetch(url)
         .then(function(response){
             return response.json()
         })
@@ -27,4 +50,10 @@ function retrieveData(){
                 main.innerText += person.email+"\n" // "\n" is used for creating line breaks, similar to the <br> tag
             }
         })
+    }
+}
+
+function resetNames(){
+    const main = document.querySelector("main")
+    main.innerText = ""
 }
